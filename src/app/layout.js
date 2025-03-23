5a2cd8c11cdaddba51d2a82b7e5357e333353089
Karthik Shetty <54941058+karthikshetty26@users.noreply.github.com>
2025-03-23 19:10:10 +0530

    Import front through next/font/google

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,13 @@
 import "./globals.css";
 import NavbarUi from '@/component/navbar_ui/navbar_ui'
 import Script from "next/script";
+import { Poppins } from "next/font/google"
+
+const poppinsSans = Poppins({
+  subsets: ["latin"],
+  weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
+  variable: "--font-primary",
+});
 
 export const metadata = {
   metadataBase: new URL('https://connect.karthikshetty.info'),
@@ -43,11 +50,6 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <head>
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
-        <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet" />
-        <link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet" />
-
         {/* <Script async src="https://www.googletagmanager.com/gtag/js?id=G-9J1RVTSWHQ" />
         <Script id='google-analytics' strategy='afterInteractive'>{`
         window.dataLayer = window.dataLayer || [];
@@ -57,7 +59,7 @@ export default function RootLayout({ children }) {
         gtag('config', 'G-9J1RVTSWHQ');
         `}</Script> */}
       </head>
-      <body>
+      <body className={poppinsSans.variable}>
         <NavbarUi />
         {children}
       </body>
