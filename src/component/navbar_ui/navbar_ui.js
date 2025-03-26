f2f826595bd05a7a74e07a95376bb340851f15a7
Karthik Shetty <54941058+karthikshetty26@users.noreply.github.com>
2025-03-26 11:53:29 +0530

    Updated navigation

--- src/component/navbar_ui/navbar_ui.js
+++ src/component/navbar_ui/navbar_ui.js
@@ -4,7 +4,7 @@ import { useEffect, useState } from 'react';
 
 export default function NavbarUi() {
     const [theme, setTheme] = useState('dark');
-    const [toastMessage, setToastMessage] = useState("URL copied successfully!");
+    const toastMessage = "Link copied! Connect with me anytime...";
     const [copySuccess, setCopySuccess] = useState(false);
 
     // Function to copy the current page URL to the clipboard
@@ -13,7 +13,7 @@ export default function NavbarUi() {
             .writeText(window.location.href)
             .then(() => {
                 setCopySuccess(true);
-                setTimeout(() => setCopySuccess(false), 4000); // runs after 1 sec
+                setTimeout(() => setCopySuccess(false), 4000); // runs after 4 sec
             })
             .catch((err) => console.error("Error copying text: ", err)); // Error handling
     }
@@ -54,23 +54,17 @@ export default function NavbarUi() {
     return (<nav className={NAVCSS.nav}>
         {/* Theme toggle */}
         <button className={NAVCSS.theme_selection} onClick={toggleTheme}>
-            {theme === 'light' ? 'Dark' : 'Light'}
-            <span className={NAVCSS.dark_light_icon}>
-                {theme === 'light' ? (
-                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor"><path d="M12 22C17.5228 22 22 17.5228 22 12C22 6.47715 17.5228 2 12 2C6.47715 2 2 6.47715 2 12C2 17.5228 6.47715 22 12 22Z"></path></svg>
-                ) : (
-                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor"><path d="M11.3807 2.01886C9.91573 3.38768 9 5.3369 9 7.49999C9 11.6421 12.3579 15 16.5 15C18.6631 15 20.6123 14.0843 21.9811 12.6193C21.6613 17.8537 17.3149 22 12 22C6.47715 22 2 17.5228 2 12C2 6.68514 6.14629 2.33869 11.3807 2.01886Z"></path></svg>
-                )}
-
-            </span>
+            {theme === 'light' ? (
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor"><path d="M10 7C10 10.866 13.134 14 17 14C18.9584 14 20.729 13.1957 21.9995 11.8995C22 11.933 22 11.9665 22 12C22 17.5228 17.5228 22 12 22C6.47715 22 2 17.5228 2 12C2 6.47715 6.47715 2 12 2C12.0335 2 12.067 2 12.1005 2.00049C10.8043 3.27098 10 5.04157 10 7ZM4 12C4 16.4183 7.58172 20 12 20C15.0583 20 17.7158 18.2839 19.062 15.7621C18.3945 15.9187 17.7035 16 17 16C12.0294 16 8 11.9706 8 7C8 6.29648 8.08133 5.60547 8.2379 4.938C5.71611 6.28423 4 8.9417 4 12Z"></path></svg>
+            ) : (
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor"><path d="M12 18C8.68629 18 6 15.3137 6 12C6 8.68629 8.68629 6 12 6C15.3137 6 18 8.68629 18 12C18 15.3137 15.3137 18 12 18ZM12 16C14.2091 16 16 14.2091 16 12C16 9.79086 14.2091 8 12 8C9.79086 8 8 9.79086 8 12C8 14.2091 9.79086 16 12 16ZM11 1H13V4H11V1ZM11 20H13V23H11V20ZM3.51472 4.92893L4.92893 3.51472L7.05025 5.63604L5.63604 7.05025L3.51472 4.92893ZM16.9497 18.364L18.364 16.9497L20.4853 19.0711L19.0711 20.4853L16.9497 18.364ZM19.0711 3.51472L20.4853 4.92893L18.364 7.05025L16.9497 5.63604L19.0711 3.51472ZM5.63604 16.9497L7.05025 18.364L4.92893 20.4853L3.51472 19.0711L5.63604 16.9497ZM23 11V13H20V11H23ZM4 11V13H1V11H4Z"></path></svg>
+            )}
         </button>
 
         {/* Copy site */}
         <button className={NAVCSS.copy_site} onClick={copyToClipboard}>
-            Copy Link
-            <span className={NAVCSS.copy_icon}><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor"><path d="M6.9998 6V3C6.9998 2.44772 7.44752 2 7.9998 2H19.9998C20.5521 2 20.9998 2.44772 20.9998 3V17C20.9998 17.5523 20.5521 18 19.9998 18H16.9998V20.9991C16.9998 21.5519 16.5499 22 15.993 22H4.00666C3.45059 22 3 21.5554 3 20.9991L3.0026 7.00087C3.0027 6.44811 3.45264 6 4.00942 6H6.9998ZM8.9998 6H16.9998V16H18.9998V4H8.9998V6Z"></path></svg></span>
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor"><path d="M6.9998 6V3C6.9998 2.44772 7.44752 2 7.9998 2H19.9998C20.5521 2 20.9998 2.44772 20.9998 3V17C20.9998 17.5523 20.5521 18 19.9998 18H16.9998V20.9991C16.9998 21.5519 16.5499 22 15.993 22H4.00666C3.45059 22 3 21.5554 3 20.9991L3.0026 7.00087C3.0027 6.44811 3.45264 6 4.00942 6H6.9998ZM5.00242 8L5.00019 20H14.9998V8H5.00242ZM8.9998 6H16.9998V16H18.9998V4H8.9998V6Z"></path></svg>
         </button>
-
 
         <div className={`${NAVCSS.toast_div} ${copySuccess ? NAVCSS.show : ''}`}>
             <div className={NAVCSS.toast_messages}>{toastMessage}</div>
