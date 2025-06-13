773a2f6ddfa15fb1b37aca900b9d5422af5c31d9
Karthik Shetty <54941058+karthikshetty26@users.noreply.github.com>
2025-06-13 17:16:57 +0530

    404 page design updated

--- src/app/not-found.js
+++ src/app/not-found.js
@@ -4,9 +4,11 @@ import Link from 'next/link';
 export default function NotFound() {
     return (<>
         <main className={ERROR.main}>
-            <h1 className={ERROR.error_h1}>404</h1>
-            <p className={ERROR.error_p}>Don&lsquo;t go here and there! Let&lsquo;s start from the beginning.</p>
-            <Link className={ERROR.error_a} href="/">Back to Home</Link>
+            <section className={ERROR.error_section}>
+                <h1 className={ERROR.error_h1}>404</h1>
+                <p className={ERROR.error_p}>Don&lsquo;t go here and there! Let&lsquo;s start from the beginning.</p>
+                <Link className={ERROR.error_a} href="/">Back to Base</Link>
+            </section>
         </main>
     </>
     )
